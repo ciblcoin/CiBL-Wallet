0bbd63c63ff8fc4086d307ad6f89761e2a40a20a
ciblcoin <ciblcoin@gmail.com>
2025-12-28 17:28:45 +0330

    Update for Cloudflare Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,21 +1,40 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  
   experimental: {
     turbo: false
   },
+  
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
         fs: false,
         path: false,
-        crypto: false
+        crypto: false,
+        stream: require.resolve('stream-browserify'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        os: require.resolve('os-browserify/browser'),
+        vm: require.resolve('vm-browserify')
       };
     }
+    
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    });
+    
     return config;
   },
+  
   typescript: {
     ignoreBuildErrors: true,
+  },
+  
+  images: {
+    unoptimized: true,
   }
 };
+
 module.exports = nextConfig;
