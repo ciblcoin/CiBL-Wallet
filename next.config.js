04bd38ecb94317f6fb90875692315615afdaf3dd
ciblcoin <ciblcoin@gmail.com>
2025-12-30 14:07:02 +0330

    Build successful and local test passed

--- next.config.js
+++ next.config.js
@@ -7,8 +7,20 @@ const nextConfig = {
       config.resolve.fallback = {
         fs: false,
         path: false,
-        crypto: false
+        crypto: require.resolve("crypto-browserify"),
+        stream: require.resolve("stream-browserify"),
+        buffer: require.resolve("buffer/"),
       };
+    }
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    });
+    if (!isServer) {
+      const ProvidePlugin = config.plugins.find(plugin => plugin.constructor.name === 'ProvidePlugin');
+      if (ProvidePlugin) {
+        ProvidePlugin.definitions.Buffer = ['buffer', 'Buffer'];
+      }
     }
     return config;
   },
