20f625f86fb62478ee4c6f26206109e3f08aba2d
CIB <ciblcoin@gmail.com>
2026-01-07 18:55:03 +0330

    Delete scripts/testEnv.js

--- scripts/testEnv.js
+++
@@ -1,8 +0,0 @@
-import { readFileSync } from 'fs'
-const env = readFileSync('.env.local', 'utf8')
-console.log('File content:')
-console.log(env)
-console.log('\nParsed:')
-env.split('\n').forEach((line, i) => {
-  console.log(Line ${i}: "${line}")
-})
